9ef3ae2aa5a02b98e1d92d9d0fc11d2e2835c18e
Niklas Flahavan <niklas@rayatheapp.com>
2020-11-14 12:10:19 -0600

    Initial Commit

---
+++ pompom/pompomApp.swift
@@ -0,0 +1,17 @@
+//
+//  pompomApp.swift
+//  pompom
+//
+//  Created by Niklas Flahavan on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct pompomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
