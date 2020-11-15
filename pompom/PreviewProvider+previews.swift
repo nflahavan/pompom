ea6e2e753a37e1fe1b6df49d833a9dc4a745e833
Niklas Flahavan <niklas@rayatheapp.com>
2020-11-15 13:19:26 -0600

    splash_screen: PreviewProvider+previews

---
+++ pompom/PreviewProvider+previews.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewProvider+previews.swift
+//  pompom
+//
+//  Created by Niklas Flahavan on 11/15/20.
+//
+
+import SwiftUI
+
+extension PreviewProvider {
+  @ViewBuilder
+  static func previews<T: View>(of view: T) -> some View {
+    view
+      .previewDevice("iPhone SE (2nd generation)")
+    view
+      .previewDevice("iPhone 12 mini")
+    view
+      .previewDevice("iPhone 12 Pro")
+    view
+      .previewDevice("iPhone 12 Pro Max")
+    view
+      .previewDevice("iPad Pro (12.9-inch) (4th generation)")
+  }
+}
