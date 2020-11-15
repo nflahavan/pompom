e6392e9b0b55f40295c3c09c3e5f1b11c1b23031
Niklas Flahavan <niklas@rayatheapp.com>
2020-11-15 13:57:13 -0600

    splash_screen: colors

--- pompom/Theme.swift
+++ pompom/Theme.swift
@@ -9,4 +9,7 @@ import SwiftUI
 
 enum Theme {
   static let blue: Color = .init("blue_theme")
+  static let red: Color = .init("red_theme")
+  static let green: Color = .init("green_theme")
+  static let yellow: Color = .init("yellow_theme")
 }
